9a8dde840b10b2d653d732b5abbc35ee6812a8aa
JeanCarlos <jmatochepascual@gmail.com>
2025-05-18 21:35:48 -0500

    feat: :art: Add Navbas component

---
+++ src/ui/components/Navbar.tsx
@@ -0,0 +1,46 @@
+import { NavLink } from "react-router"
+
+export const Navbar = () => {
+  return (
+    <nav className="py-4 bg-gray-900">
+      <div className="w-11/12 max-w-7xl mx-auto flex justify-between">
+        <div className="flex gap-2 items-center">
+          <NavLink
+            to="marvel"
+            className={({ isActive }) => `${isActive ? 'text-white font-semibold' : 'text-gray-300'}`} >
+            Marvel
+          </NavLink>
+
+          <NavLink
+            to="dc"
+            className={({ isActive }) => `${isActive ? 'text-white font-semibold' : 'text-gray-300'}`} >
+            DC
+          </NavLink>
+
+          <NavLink
+            to="search"
+            className={({ isActive }) => `${isActive ? 'text-white font-semibold' : 'text-gray-300'}`} >
+            Search
+          </NavLink>
+
+          <NavLink
+            to="/login"
+            className={({ isActive }) => `${isActive ? 'text-white font-semibold' : 'text-gray-300'}`} >
+            Login
+          </NavLink>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <span className="text-blue-300">
+            JeanCarlos
+          </span>
+
+          <button
+            className="px-4 py-1 rounded-md cursor-pointer text-white bg-blue-500 hover:bg-blue-600 transition-colors duration-300">
+            Logout
+          </button>
+        </div>
+      </div >
+    </nav >
+  )
+}
